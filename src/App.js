b05aad47386e8da052bf50299146e8dcbc00fcbb
Eric Ndung'u <ericobain@gmail.com>
2019-10-07 15:29:51 +0300

    Style the landing page with bootstrap
    - also themed the entire app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Nav />
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/profile" component={Profile}></Route>
-      </>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Route exact path="/" render={props => <Home />} />
+          <Route path="/profile" component={Profile} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import { Jumbotron, Button } from "reactstrap";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Landing Page</h1>
+        <Jumbotron className="custom-jumbotron">
+          <h1 className="main-text">Hello, Welcome to Radio Africa</h1>
+          <Button color="secondary" className="custom-button" size="lg" block>
+            Sign Up / Sign In
+          </Button>
+        </Jumbotron>
       </div>
     );
   }
